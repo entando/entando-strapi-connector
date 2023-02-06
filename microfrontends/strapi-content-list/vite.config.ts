2157c0e9fbde9b1051efcf61dd3fbcad372d2ba6
Marco Corradetti <m.corradetti@entando.com>
2023-02-06 16:40:01 +0100

    add base structure of the project

---
+++ microfrontends/strapi-content-list/vite.config.ts
@@ -0,0 +1,17 @@
+import * as path from 'node:path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './src/main.tsx'),
+      name: 'strapi-content-list',
+      formats: ['umd'],
+      fileName: format => `strapi-content-list.${format}.js`
+    },
+    outDir: path.resolve(__dirname, './build')
+  }
+})
