770286b786c53b6f688b65de7c7b01972be7d212
Marco Corradetti <m.corradetti@entando.com>
2023-02-10 12:23:39 +0100

    update vite config

--- microfrontends/strapi-content-list-config/vite.config.ts
+++ microfrontends/strapi-content-list-config/vite.config.ts
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   envPrefix: 'STRAPI_CONNECTOR',
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+  },
   build: {
     lib: {
       entry: path.resolve(__dirname, './src/main.tsx'),
--- microfrontends/strapi-content-list/vite.config.ts
+++ microfrontends/strapi-content-list/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+  },
   build: {
     lib: {
       entry: path.resolve(__dirname, './src/main.tsx'),
