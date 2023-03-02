70d9751f0ef3a4dfca1fe2dbacc96e3600fbe410
Giovanni Prete <g.prete@entando.com>
2023-03-02 16:22:19 +0100

    update excludedPaths and add callback option to keycloak.js

--- microservices/strapi-connector-ms/plugins/keycloak.js
+++ microservices/strapi-connector-ms/plugins/keycloak.js
@@ -2,19 +2,20 @@ const fastifyPlugin = require('fastify-plugin')
 const keycloak = require('fastify-keycloak-adapter')
 
 module.exports = fastifyPlugin(async function (fastify, opts, done) {
-    const keycloakUrl = opts.KEYCLOAK_AUTH_URL + '/realms/' + opts.KEYCLOAK_REALM
+    const keycloakUrl = opts.KEYCLOAK_AUTH_URL.replace("http://", "").replace("https://", "") + '/realms/' + opts.KEYCLOAK_REALM
     var appOrigin = opts.APP_ORIGIN
     var excludedPaths = []
     if (opts.SERVER_SERVLET_CONTEXT_PATH && opts.SERVER_SERVLET_CONTEXT_PATH != '/' ) {
         appOrigin = opts.APP_ORIGIN + opts.SERVER_SERVLET_CONTEXT_PATH
-        excludedPaths.push(opts.SERVER_SERVLET_CONTEXT_PATH + "/connect/*")
+        excludedPaths.push(opts.SERVER_SERVLET_CONTEXT_PATH + "/connect/**")
     }
     const kyecloakOpts = {
         appOrigin: appOrigin,
-        keycloakSubdomain: keycloakUrl.replace("http://", "").replace("https://", ""),
+        keycloakSubdomain: keycloakUrl,
         clientId: opts.KEYCLOAK_CLIENT_ID,
         clientSecret: opts.KEYCLOAK_CLIENT_SECRET,
-        excludedPatterns: excludedPaths
+        excludedPatterns: excludedPaths,
+        callback: appOrigin + '/api/health'
     }
     fastify.register(keycloak, kyecloakOpts)
     done()
