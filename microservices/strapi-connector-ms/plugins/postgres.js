db3f2a5b1c8d1eed7fa05608bad3aaa7f860b156
Giovanni Prete <g.prete@entando.com>
2023-03-02 16:22:56 +0100

    clean postgres.js

--- microservices/strapi-connector-ms/plugins/postgres.js
+++ microservices/strapi-connector-ms/plugins/postgres.js
@@ -1,10 +1,9 @@
 const fastifyPlugin = require('fastify-plugin')
+const postgres = require('@fastify/postgres')
 
 module.exports = fastifyPlugin(async function (fastify, opts, done) {
     const dbUrl = opts.SPRING_DATASOURCE_URL.slice(18)
     const connectionString = 'postgres://' + opts.SPRING_DATASOURCE_USERNAME + ":" + opts.SPRING_DATASOURCE_PASSWORD + "@" + dbUrl
-    fastify.register(require('@fastify/postgres'), {
-        connectionString: connectionString
-    })
+    fastify.register(postgres, { connectionString: connectionString })
     done()
 })
