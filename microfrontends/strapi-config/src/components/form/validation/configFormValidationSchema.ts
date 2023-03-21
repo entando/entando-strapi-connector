432a56250d3887bd35055887b353adf6bbf9beb3
DMinnai <d.minnai@entando.com>
2023-03-21 12:39:58 +0100

    amend reg expr on get protocols

--- microfrontends/strapi-config/src/components/form/validation/configFormValidationSchema.ts
+++ microfrontends/strapi-config/src/components/form/validation/configFormValidationSchema.ts
@@ -3,7 +3,7 @@ import * as Yup from "yup"
 export const configFormValidationSchema = Yup.object().shape({
   connectionUrl: Yup.string()
     .matches(
-      /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+      /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/,
       "enterAValidUrl"
     )
     .required("urlIsRequired"),
