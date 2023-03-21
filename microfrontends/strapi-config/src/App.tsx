b82cdb0f05d8df83072134c0fac95a47a625b067
davdet <dv.dt@hotmail.com>
2023-03-21 11:42:55 +0100

    Minor fix

--- microfrontends/strapi-config/src/App.tsx
+++ microfrontends/strapi-config/src/App.tsx
@@ -25,7 +25,7 @@ export function App() {
     if (window?.entando?.globals?.lang) {
       setLocale(window.entando.globals.lang)
     }
-  }, [setLocale])
+  }, [])
 
   return (
     <IntlProvider locale={locale} messages={messages[locale]}>
