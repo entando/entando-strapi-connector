e47883bb189083ff0a31915e93164942599ad323
DMinnai <d.minnai@entando.com>
2023-03-21 09:45:16 +0100

    amend run commands with install

--- microfrontends/strapi-config/src/App.tsx
+++ microfrontends/strapi-config/src/App.tsx
@@ -19,7 +19,7 @@ const messages: Message = {
 export function App() {
   const { matchPath } = useEPCRouter()
 
-  const [locale, setLocale] = useState<string>("")
+  const [locale, setLocale] = useState<string>("en")
 
   return (
     <IntlProvider locale={locale} messages={messages[locale]}>
