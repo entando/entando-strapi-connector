b1a32df5a589e804dd2045aef0fd604b682c8e7f
Giovanni Prete <g.prete@entando.com>
2023-03-24 17:53:44 +0100

    Change tap with vitest

--- microservices/strapi-connector-ms/README.md
+++ microservices/strapi-connector-ms/README.md
@@ -1,5 +1,18 @@
 # Strapi Connector Microservice
 
+## Configuration
+
+A sample configuration is provided in the `.env.sample` file. When running with `NODE_ENV=development` the service will look up for a `.env.local` file. Environment variables will always have priority over .env files.
+
+The configuration variables are managed by the package `@fastify/env` which is a wrapper around [env-schema](https://www.npmjs.com/package/env-schema).
+
+### Variables
+
+* The `KEYCLOAK_`, `SPRING_DATASOURCE` and `SERVER_SERVLET_CONTEXT_PATH` are managed by the Entando operator.
+* `API_CONFIG_TABLE` is the table name used by the service. The service will try to create the table if at startup.
+* Additionally `JWT_PUB_KEY` could be set. If set the service will use the provided key to validate the JWT tokens instead of the `protocol/openid-connect/userinfo` endpoint of the keycloak service.\
+  The variable should be set as a single line **without** the header and footer, the value could be retrived from  `protocol/openid-connect/certs`.
+
 ## Available Scripts
 
 In the project directory, you can run:
@@ -16,6 +29,8 @@ For production mode
 ### `npm run test`
 
 Run the test cases.
+
+A running PostgreSQL instance is needed to run the tests, set the connections parameters in the `test/.env.test` file. The instance started by `ent bundle svc start postgresql` will work for testing purpose.
 
 ## Learn More
 
--- microservices/strapi-connector-ms/test/api.health.test.js
+++ microservices/strapi-connector-ms/test/api.health.test.js
@@ -1,14 +1,38 @@
-import test from 'tap'
+import { test, expect, afterEach } from 'vitest'
 import buildApp from '../app.js'
 
-test('requests the "/api/health" route', async t => {
-  const app = buildApp()
+let app
 
+afterEach(async () => {
+  await app.close()
+})
+
+test('/api/health should be successful with a working DB connection', async () => {
+  process.env.SPRING_DATASOURCE_PASSWORD = 'entando_strapi_connector_password'
+  process.env.SPRING_DATASOURCE_USERNAME = 'entando_strapi_connector_user'
+  process.env.SPRING_DATASOURCE_URL = 'jdbc:postgresql://localhost:5432/entando_strapi_connector'
+  app = buildApp()
+  await app.ready()
+  const response = await app.inject({
+    method: 'GET',
+    url: '/api/health'
+  })
+  console.log(response.json())
+
+  expect(response.statusCode).toEqual(200)
+  expect(response.json()).toEqual({ status: { server: "UP", db: "UP" }, error: null })
+})
+
+test('/api/health should fail with a bad DB connection', async () => {
+  process.env.SPRING_DATASOURCE_URL = 'jdbc:postgresql://localhost:5432/this_should_not_work'
+
+  app = buildApp()
+  await app.ready()
   const response = await app.inject({
     method: 'GET',
     url: '/api/health'
   })
 
-  t.equal(response.statusCode, 200, 'returns a status code of 200')
-  t.same(response.json(), { status: 'UP' }, 'returns a body with { status: \'UP\'}')
+  expect(response.statusCode).toEqual(503)
+  expect(response.json().status).toEqual({ server: "UP", db: "DOWN" })
 })
