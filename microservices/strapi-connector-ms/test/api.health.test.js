d7fcb009c6f3ae8bb73593c2c82350b0f84a511a
Giovanni Prete <g.prete@entando.com>
2023-03-27 10:48:54 +0200

    Remove unused code from api.health.test.js

--- microservices/strapi-connector-ms/test/api.health.test.js
+++ microservices/strapi-connector-ms/test/api.health.test.js
@@ -1,5 +1,6 @@
 import { test, expect, afterEach } from 'vitest'
 import buildApp from '../app.js'
+import { appConstants } from '../config/appConstants.js'
 
 let app
 
@@ -8,22 +9,19 @@ afterEach(async () => {
 })
 
 test('/api/health should be successful with a working DB connection', async () => {
-  process.env.SPRING_DATASOURCE_PASSWORD = 'entando_strapi_connector_password'
-  process.env.SPRING_DATASOURCE_USERNAME = 'entando_strapi_connector_user'
-  process.env.SPRING_DATASOURCE_URL = 'jdbc:postgresql://localhost:5432/entando_strapi_connector'
   app = buildApp()
   await app.ready()
   const response = await app.inject({
     method: 'GET',
     url: '/api/health'
   })
-  console.log(response.json())
 
-  expect(response.statusCode).toEqual(200)
+  expect(response.statusCode).toEqual(appConstants.HTTP_CODE_OK)
   expect(response.json()).toEqual({ status: { server: "UP", db: "UP" }, error: null })
 })
 
 test('/api/health should fail with a bad DB connection', async () => {
+  // Overwrite .env.test SPRING_DATASOURCE_URL
   process.env.SPRING_DATASOURCE_URL = 'jdbc:postgresql://localhost:5432/this_should_not_work'
 
   app = buildApp()
@@ -33,6 +31,6 @@ test('/api/health should fail with a bad DB connection', async () => {
     url: '/api/health'
   })
 
-  expect(response.statusCode).toEqual(503)
+  expect(response.statusCode).toEqual(appConstants.HTTP_CODE_SERVICE_UNAVAILABLE)
   expect(response.json().status).toEqual({ server: "UP", db: "DOWN" })
 })
