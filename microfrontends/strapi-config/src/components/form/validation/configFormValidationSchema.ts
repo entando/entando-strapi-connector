72cb67a4a99d64efed1a801bca9e72fecba0e74e
davdet <dv.dt@hotmail.com>
2023-03-29 10:39:08 +0200

    Fix regex

--- microfrontends/strapi-config/src/components/form/validation/configFormValidationSchema.ts
+++ microfrontends/strapi-config/src/components/form/validation/configFormValidationSchema.ts
@@ -2,7 +2,7 @@ import * as Yup from "yup"
 
 export const configFormValidationSchema = Yup.object().shape({
   connectionUrl: Yup.string()
-    .matches(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/, "invalidConfigUrl")
+    .matches(/^https?:\/\/[a-z0-9]+(?:[-.][a-z0-9]+)*(?::[0-9]{1,5})?(?:\/[^\/\r\n]+)*\.[a-z]{2,5}(?:[?#]\S*)?$/, "invalidConfigUrl")
     .required("urlIsRequired"),
   connectionToken: Yup.string().required("tokenIsRequired")
 })
